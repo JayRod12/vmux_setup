1d337a775c75a35c20c46c7f307af879837611ef
Jaime Rodriguez <jaime.rodriguez1295@gmail.com>
2017-10-27 22:44:15 +0000

    Add .gitconfig to the script

--- setup.sh
+++ setup.sh
@@ -9,31 +9,29 @@ fi
 if [ -f ~/.tmux.conf ]; then
   cp ~/.tmux.conf ~/.tmux.conf.old
 fi
-TMUX_PANE_SELECTION=" # Vim style pane selection bind h select-pane -L
-bind j select-pane -D
-bind k select-pane -U
-bind l select-pane -R"
 
-MAC="set -g mouse on"
-LINUX="
+if [ -f ~/.gitconfig ]; then
+  cp ~/.gitconfig ~/.gitconfig.old
+fi
+
+read -n1 -p "Are you on Linux [L/l] or Mac [M/m] ? " doit
+echo
+
+case $doit in
+  L|l)
+    echo "Copying Linux configurations for Vim and Tmux.."
+cat >~/.tmux.conf <<EOL
 setw -g mode-mouse on
 set -g mouse-select-window on
 set -g mouse-select-pane on
 set -g mouse-resize-pane on
-set -g mouse-utf on"
-
-
-read -n1 -p "Are you on Linux [L/l] or Mac [M/m] ? " doit
-echo
-case $doit in
-  L|l)
-    echo "Copying Linux configurations for Vim and Tmux.."
-    echo $LINUX > ~/.tmux.conf
+set -g mouse-utf on
+EOL
     cp vimrc-linux ~/.vimrc 
     ;;
   M|m)
     echo "Copying Mac configurations for Vim and Tmux.."
-    echo $MAC > ~/.tmux.conf
+    echo "set -g mouse on" > ~/.tmux.conf
     cp vimrc-mac ~/.vimrc
     ;;
   *)
@@ -41,14 +39,22 @@ case $doit in
     exit -1
     ;;
 esac
+cat >>~/.tmux.conf <<EOL
+# Vim style pane selection bind h select-pane -L
+bind j select-pane -D
+bind k select-pane -U
+bind l select-pane -R
+EOL
 
-echo $TMUX_PANE_SELECTION >> ~/.tmux.conf
-
+# Install Vundle, Vim package manager
 if [ ! -d ~/.vim/bundle/Vundle.vim/ ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 else
   echo "Vundle.vim already installed."
 fi
-
 vim +PluginInstall +qall
-echo "Updated ~/.vimrc ~/.tmux.conf, previous versions stored in [FILENAME].old"
+
+# Update .gitconfig
+cp gitconfig ~/.gitconfig
+
+echo "Updated ~/.vimrc ~/.tmux.conf ~/.gitconfig, previous versions stored in [FILENAME].old"
